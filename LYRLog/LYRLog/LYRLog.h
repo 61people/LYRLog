ba6a39894df83aa8db2c2e7a22e9b958d15817ed
61people <lyrlyr_422@163.com>
2020-08-19 09:07:40 +0800

    [U]change author

--- LYRLog/LYRLog/LYRLog.h
+++ LYRLog/LYRLog/LYRLog.h
@@ -2,7 +2,7 @@
 //  LYRLog.h
 //  LYRLog
 //
-//  Created by LYR on 2020/8/18.
+//  Created by 61people on 2020/8/18.
 //  Copyright © 2020 LYR. All rights reserved.
 //
 
--- LYRLog/LYRLog/LYRLogEngine.h
+++ LYRLog/LYRLog/LYRLogEngine.h
@@ -2,7 +2,7 @@
 //  LYRLogEngine.h
 //  LYRLog
 //
-//  Created by 刘奕任 on 2020/8/18.
+//  Created by 61people on 2020/8/18.
 //  Copyright © 2020 LYR. All rights reserved.
 //
 
